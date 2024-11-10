b852886c6296d50a79a1ab62ac2a3c7e829ba2b0
KeithVreeland3 <156938617+KeithVreeland3@users.noreply.github.com>
2024-11-10 14:41:16 -0500

    Added randomization tests
    for back+front temp, traffic light, and distance to lead vehicle

--- backend/simulation.py
+++ backend/simulation.py
@@ -12,9 +12,14 @@ test_data = [
     (0x01, 85.0),  # battery_soc (e.g., 85%)
     (0x02, 37.5),  # temperature (e.g., 37.5°C)
     (0x03, 45.2),  # hv_battery_pack_temp
-    (0x04, 50.1),  # edu_reported_temp
+    (0x04, 50.1),  # back_edu_reported_temp
+    (0x0F, 50.1),  # front_edu_reported_temp
     (0x05, 1.0),   # drive_mode_active (e.g., "Drive")
-    (0x0B, 69.0),  # CACC
+#0x06-08 are the switches
+    (0x09, 20.0),  # distance_to_lead_vehicle
+    (0x0A, 0.0),   # traffic_light_state (e.g "Green")
+    (0x0B, 69.0),  # CACC 
+    #0x0C-0E are yet to be implemented in tcp_client.py
 ]
 
 # Start the simulated TCP server
@@ -36,10 +41,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                     value = random.uniform(35.5, 39.5)
                 elif identifier == 0x03:  # hv_battery_pack_temp (± 3°C)
                     value = random.uniform(42.2, 48.2)
-                elif identifier == 0x04:  # edu_reported_temp (± 3°C)
+                elif identifier == 0x04:  # back_edu_reported_temp (± 3°C)
+                    value = random.uniform(47.1, 53.1)
+                elif identifier == 0x0F:  # front_edu_reported_temp (± 3°C)
                     value = random.uniform(47.1, 53.1)
                 elif identifier == 0x05:  # drive_mode_active (fixed value for simplicity)
                     value = 1.0
+                elif indentifier == 0x09: # distance_to_lead_vehicle (20.0m ± 5.0m)
+                    value = random.uniform(15.0, 25.0)
+                elif indentifier == 0x0A: # traffic_light_state (e.g. "Green")
+                    value = random.uniform(0.0, 2.0)
                 elif identifier == 0x0B:  # CACC mileage (e.g., ± 10 miles)
                     value = random.uniform(59.0, 79.0)
 
@@ -51,4 +62,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                 conn.sendall(data)
                 print(f"Sent: identifier={identifier}, value={value}")
 
-                time.sleep(2)  # Wait 2 seconds between sending messages
+                time.sleep(1)  # Wait 1 seconds between sending messages
